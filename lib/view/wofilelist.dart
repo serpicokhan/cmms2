9d052da3f9c4772a72bef3c962c4ab87dd64ab67
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2021-09-14 15:39:18 +0430

    789

--- lib/view/wofilelist.dart
+++ lib/view/wofilelist.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:cmms2/glob.dart';
 import 'package:cmms2/models/wofile.dart';
@@ -12,6 +13,8 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+import 'package:uuid/uuid.dart';
 
 import 'fab.dart';
 
@@ -22,16 +25,116 @@ class ListViewWorkorderFile extends StatefulWidget {
   _ListViewWorkorderFileState createState() => _ListViewWorkorderFileState();
 }
 
+downloadFile2(String url, String dir, context) async {
+  var httpClient = http.Client();
+  var uri = Uri.parse(url);
+  var request = new http.Request('GET', Uri.parse(url));
+  var response = httpClient.send(request);
+  // String dir = await ExtStorage.getExternalStoragePublicDirectory(ExtStorage
+  //     .DIRECTORY_DOWNLOADS); //(await getApplicationDocumentsDirectory()).path;
+  final filename = Uri.decodeFull(path.basename(url));
+  final extension = path.extension(url);
+
+  List<List<int>> chunks = [];
+  int downloaded = 0;
+
+  response.asStream().listen((http.StreamedResponse r) {
+    r.stream.listen((List<int> chunk) {
+      // Display percentage of completion
+      // debugPrint('downloadPercentage: ${downloaded / r.contentLength * 100}');
+
+      chunks.add(chunk);
+      downloaded += chunk.length;
+    }, onDone: () async {
+      // Display percentage of completion
+      // debugPrint('downloadPercentage: ${downloaded / r.contentLength * 100}');
+
+      // Save the file
+      File file = new File('$dir/$filename');
+      try {
+        if (!await file.exists()) {
+          final Uint8List bytes = Uint8List(r.contentLength!.toInt());
+          int offset = 0;
+          for (List<int> chunk in chunks) {
+            bytes.setRange(offset, offset + chunk.length, chunk);
+            offset += chunk.length;
+          }
+          await file.writeAsBytes(bytes);
+          return;
+        } else {
+          File file2 = new File('$dir/' + Uuid().v4() + ".$extension");
+          final Uint8List bytes = Uint8List(r.contentLength!.toInt());
+          int offset = 0;
+          for (List<int> chunk in chunks) {
+            bytes.setRange(offset, offset + chunk.length, chunk);
+            offset += chunk.length;
+          }
+          await file2.writeAsBytes(bytes);
+        }
+        _showAction2(context, 'completed');
+      } catch (e) {
+        _showAction2(context, e.toString());
+      }
+    });
+  });
+}
+
+void _showAction2(BuildContext context, String index) {
+  showDialog<void>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        content: Text(index),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('CLOSE'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future<List<WorkorderFile>> _fetchwoFile(id) async {
+  // appDocDir = await getApplicationDocumentsDirectory();
+  // _requestAppDocumentsDirectory().then((value) => dir = value.path);
+
+  final response = await http
+      .get(Uri.parse(ServerStatus.ServerAddress + '/api/v1/WoFiles/$id/'));
+
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(utf8.decode(response.bodyBytes));
+    return jsonResponse
+        .map((task) => new WorkorderFile.fromJson(task))
+        .toList();
+  } else {
+    throw Exception('Failed  to load wopart from API');
+  }
+}
+
 class _ListViewWorkorderFileState extends State<ListViewWorkorderFile>
     with AutomaticKeepAliveClientMixin<ListViewWorkorderFile> {
   late Directory appDirectory;
   List<String> records = [];
-
+  List<String> rec = [];
   @override
   void initState() {
     super.initState();
+    _fetchwoFile(widget.id)
+        .then((value) => rec = value
+            .where((element) => element.woFile.contains('.aac'))
+            .map((e) => ServerStatus.ServerAddress + e.woFile)
+            .toList())
+        .then((value) => setState(() {}));
+
     getApplicationDocumentsDirectory().then((value) {
       appDirectory = value;
+      //   for (var i = 0; i < rec.length; i++) {
+      //     downloadFile2(ServerStatus.ServerAddress + '/' + rec[i],
+      //         appDirectory.path, context);
+      // }
+
       appDirectory.list().listen((onData) {
         if (onData.path.contains('.aac')) records.add(onData.path);
       }).onDone(() {
@@ -50,14 +153,22 @@ class _ListViewWorkorderFileState extends State<ListViewWorkorderFile>
   String lastFile = '';
   _onRecordComplete() {
     records.clear();
+
     appDirectory.list().listen((onData) {
       if (onData.path.contains('.aac')) {
         records.add(onData.path);
         lastFile = onData.path;
       }
     }).onDone(() {
-      records.sort();
-      records = records.reversed.toList();
+      // records.sort();
+      // records = records.reversed.toList();
+      _fetchwoFile(widget.id)
+          .then((value) => records = value
+              .where((element) => element.woFile.contains('.aac'))
+              .map((e) => ServerStatus.ServerAddress + e.woFile)
+              .toList())
+          .then((value) => setState(() {}));
+
       setState(() {});
       uploadFile(lastFile, widget.id);
     });
@@ -171,26 +282,9 @@ class _WorkorderFileListViewState extends State<WorkorderFileListView> {
   // late Directory appDocDir;
   // Future<Directory?>? _appDocumentsDirectory;
   late String dir;
-  Future<Directory> _requestAppDocumentsDirectory() async {
-    return getApplicationDocumentsDirectory();
-  }
-
-  Future<List<WorkorderFile>> _fetchwoFile(id) async {
-    // appDocDir = await getApplicationDocumentsDirectory();
-    _requestAppDocumentsDirectory().then((value) => dir = value.path);
-
-    final response = await http
-        .get(Uri.parse(ServerStatus.ServerAddress + '/api/v1/WoFiles/$id/'));
-
-    if (response.statusCode == 200) {
-      List jsonResponse = json.decode(utf8.decode(response.bodyBytes));
-      return jsonResponse
-          .map((task) => new WorkorderFile.fromJson(task))
-          .toList();
-    } else {
-      throw Exception('Failed  to load wopart from API');
-    }
-  }
+  // Future<Directory> _requestAppDocumentsDirectory() async {
+  //   return getApplicationDocumentsDirectory();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -213,6 +307,8 @@ class _WorkorderFileListViewState extends State<WorkorderFileListView> {
     return ListView.builder(
         itemCount: data.length,
         itemBuilder: (context, index) {
+          //here ....
+          // if (data[index].woFile.contains('.aac')) records.add(onData.path);
           return newMethod(context, data[index]);
         });
   }
@@ -226,15 +322,19 @@ class _WorkorderFileListViewState extends State<WorkorderFileListView> {
         child: InkWell(
           splashColor: Colors.blue.withAlpha(30),
           onTap: () async {
-            final taskId = await FlutterDownloader.enqueue(
-              url: ServerStatus.ServerAddress + index.woFile,
-              savedDir: dir,
-              // 'the path of directory where you want to save downloaded files',
-              showNotification:
-                  true, // show download progress in status bar (for Android)
-              openFileFromNotification:
-                  true, // click on notification to open downloaded file (for Android)
-            );
+            // final taskId = await FlutterDownloader.enqueue(
+            //   url: ServerStatus.ServerAddress + index.woFile,
+            //   savedDir: dir,
+            //   // 'the path of directory where you want to save downloaded files',
+            //   showNotification:
+            //       true, // show download progress in status bar (for Android)
+            //   openFileFromNotification:
+            //       true, // click on notification to open downloaded file (for Android)
+            // );
+            getApplicationDocumentsDirectory().then((value) {
+              downloadFile2(ServerStatus.ServerAddress + '/' + index.woFile,
+                  value.path, context);
+            });
           },
           child: Padding(
             padding: const EdgeInsets.all(8.0),
